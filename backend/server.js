4126f21553a114a34db7ea3e295f04b74d046421
john hackx <john@kali.kali.org>
2025-10-12 21:30:20 -0400

    configured frontend and backend domains in deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 // Middleware
-app.use(cors());
+const allowedOrigins = [
+  "https://cargo-express-two.vercel.app/",
+  "http://localhost:5173",
+];
+app.use(cors({ origin: allowedOrigins, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
